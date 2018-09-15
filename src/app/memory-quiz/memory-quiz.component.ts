0c6f373c3f467c7dbca4b8e593339e23ec3c5da0
Marion Schleifer <marion@Marions-MacBook-Pro-3.local>
2018-09-15 22:54:21 +0200

    finalize memory game

--- src/app/memory-quiz/memory-quiz.component.ts
+++ src/app/memory-quiz/memory-quiz.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-quiz.component.scss']
 })
 export class MemoryQuizComponent implements OnInit {
+  private cards: Card[];
   constructor() {}
 
-  ngOnInit() {}
+  private initCards(): Card[] {
+    const cardElements = document.getElementsByClassName(
+      'card'
+    ) as HTMLCollectionOf<HTMLDivElement>;
+    return Array.from(cardElements).map(el => new Card(el));
+  }
+  ngOnInit() {
+    this.cards = this.initCards();
+  }
+}
+
+class Card {
+  constructor(private el: HTMLDivElement) {
+    el.onclick = () => (this.isVisible ? this.hide() : this.reveal());
+  }
+
+  get isVisible() {
+    return !this.el.classList.contains('card-hidden');
+  }
+
+  hide(): void {
+    this.el.classList.add('card-hidden');
+  }
+
+  reveal(): void {
+    this.el.classList.remove('card-hidden');
+  }
 }
