03945ed266bfe32fb1674e8e9a9604ac287fe7fa
Marion Schleifer <marion@Marions-MacBook-Pro-3.local>
2018-09-16 00:10:32 +0200

    add flow and black screen

---
+++ src/app/black-screen/black-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-black-screen',
+  templateUrl: './black-screen.component.html',
+  styleUrls: ['./black-screen.component.css']
+})
+export class BlackScreenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
